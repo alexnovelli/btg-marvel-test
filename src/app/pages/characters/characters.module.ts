8baa32bf04a6c3605df9925f2b093db383e5745b
Alex Novelli <alexnovelli@gmail.com>
2022-04-02 22:57:25 -0300

    feat: finish main page style

--- src/app/pages/characters/characters.module.ts
+++ src/app/pages/characters/characters.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CharactersComponent } from './characters.component';
 import { CharactersRoutingModule } from './characters-routing.module';
+import { CharacterCardComponent } from './character-card/character-card.component';
 
 @NgModule({
-  declarations: [CharactersComponent],
+  declarations: [CharactersComponent, CharacterCardComponent],
   imports: [CommonModule, CharactersRoutingModule],
 })
 export class CharactersModule {}
